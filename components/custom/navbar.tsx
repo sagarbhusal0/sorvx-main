c4a64cc6558e53447c90380503fea30255c4d933
Sagar Bhusal <127485363+sagarbhusal0@users.noreply.github.com>
2025-02-13 21:02:44 +0545

    Update navbar.tsx

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -24,10 +24,10 @@ export const Navbar = async () => {
           <History user={session?.user} />
           <div className="flex flex-row gap-2 items-center">
             <Image
-              src="/images/gemini-logo.png"
+              src="https://iili.io/2mQfcCl.png"
               height={20}
               width={20}
-              alt="gemini logo"
+              alt="sorvx logo"
             />
             <div className="text-zinc-500">
               <SlashIcon size={16} />
